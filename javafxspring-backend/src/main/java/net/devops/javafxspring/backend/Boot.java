84ac5265366a7ed8e917a73716aa62e7674dda45
Kamil Kiewisz <kamkie@outlook.com>
2015-03-26 20:11:02 +0100

    better annotations for intellij

--- javafxspring-backend/src/main/java/net/devops/javafxspring/backend/Boot.java
+++ javafxspring-backend/src/main/java/net/devops/javafxspring/backend/Boot.java
@@ -2,8 +2,10 @@ package net.devops.javafxspring.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@ComponentScan
 @SpringBootApplication
 @EnableJpaRepositories
 public class Boot {
@@ -11,4 +13,5 @@ public class Boot {
     public static void main(String[] args) {
         SpringApplication.run(Boot.class, args);
     }
+
 }
