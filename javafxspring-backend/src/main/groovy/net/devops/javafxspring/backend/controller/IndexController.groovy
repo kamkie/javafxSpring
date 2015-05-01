0b97c11ec439a8881f27c1c0c7a3fc91194bf420
Kamil Kiewisz <kamkie@outlook.com>
2015-05-01 17:29:23 +0200

    add some test/temp code

--- javafxspring-backend/src/main/groovy/net/devops/javafxspring/backend/controller/IndexController.groovy
+++ javafxspring-backend/src/main/groovy/net/devops/javafxspring/backend/controller/IndexController.groovy
@@ -1,6 +1,7 @@
 package net.devops.javafxspring.backend.controller
 
 import groovy.util.logging.Slf4j
+import net.devops.javafxspring.backend.annotation.Loggable
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -8,8 +9,17 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 class IndexController {
 
+    byte[] bytes = [65, 65, 65, 65, 65] as byte[]
+
+    @Loggable
     @RequestMapping("/")
     def index() {
-        "hello word " + (1..16).findAll {it.mod(2) == 1}.join(", ")
+        "hello word " + (1..16).findAll { it.mod(2) == 1 }.join(", ")
+    }
+
+    @Loggable
+    @RequestMapping("/raw")
+    def raw() {
+        bytes
     }
 }
